1f85c82b42b89186c86a45b1338653adc8923350
piotrszleg <szlegp@gmail.com>
2020-05-26 15:45:14 +0200

    Modified parameters a little.

--- main.ml
+++ main.ml
@@ -3,19 +3,19 @@ open Evaluator;;
 open Utility;;
 open Requests;;
 
-let preset=0;;
+let preset=1;;
 let ()=
 match preset with
 0->
    evaluate_policies 
-   { page_error_rate_control_policy_delta_t=20;
+   { page_error_rate_control_policy_delta_t=30;
    page_error_rate_control_policy_lower=3;
    page_error_rate_control_policy_upper=5;
    page_error_rate_control_policy_stop=10;
    zone_model_delta_t=20;
    zone_model_c=10;
    
-   frames_count=70;
+   frames_count=90;
    processes_count=5;
    process_sizes={start=10; end_=100};
    
@@ -26,7 +26,7 @@ match preset with
    requests_per_phase={start=2; end_=20 }; }
 | 1->
    evaluate_policies 
-   { page_error_rate_control_policy_delta_t=20;
+   { page_error_rate_control_policy_delta_t=60;
    page_error_rate_control_policy_lower=3;
    page_error_rate_control_policy_upper=5;
    page_error_rate_control_policy_stop=10;
@@ -44,7 +44,7 @@ match preset with
    requests_per_phase={start=2; end_=20 }; }
 | 2->
    evaluate_policies 
-   { page_error_rate_control_policy_delta_t=20;
+   { page_error_rate_control_policy_delta_t=30;
    page_error_rate_control_policy_lower=3;
    page_error_rate_control_policy_upper=5;
    page_error_rate_control_policy_stop=10;
